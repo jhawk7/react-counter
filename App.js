22ca680f97db78b03e3980159a590902bafda9fc
jhawk7 <13038876+jhawk7@users.noreply.github.com>
2024-10-10 15:00:24 -0400

    changes

---
+++ App.js
@@ -0,0 +1,84 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Counter from './components/Counter.js';
+import { useEffect, useState } from 'react';
+import * as Haptics from 'expo-haptics';
+import ResetButton from './components/ResetButton.js';
+
+const App = () => {
+  const [counter, setCounter] = useState()
+  const [show, setShow] = useState(false)
+  const ws = new WebSocket(process.env.REACT_APP_WS_URL)
+
+  useEffect(() => {
+    getCount()
+  }, [])
+
+  ws.onerror = e => {
+    console.log(e.data)
+  }
+
+  const getCount = async () => {
+    const count = await fetch(process.env.REACT_APP_API_URL, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      }
+    })
+    .then(response => response.json())
+    .then(res => {
+      console.log(res)
+      return Number(res.count)
+    })
+    .catch(error => {
+      console.error(error)
+      return 0
+    })
+
+    setCounter(count)
+    setShow(true)
+  }
+
+  const updateCounter = async () => {
+    Haptics.impactAsync()
+    setCounter(counter +1)
+
+    ws.send(JSON.stringify({
+      event: "increment",
+    }))
+  }
+
+  const resetCounter = async () => {
+    Haptics.impactAsync()
+    setCounter(0)
+
+    ws.send(JSON.stringify({
+      event: "reset",
+    }))
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Counter</Text>
+        {show ? <Counter clicks={counter} onClick={updateCounter}/> : <Text>Loading...</Text>}
+      <ResetButton onClick={resetCounter}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0ead6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title:{
+    fontWeight: 'bold',
+    color:'black',
+    fontSize:40
+  },
+});
+
+export default App
